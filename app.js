28530da08c6ddc8548fd044b7d827b1f18535910
Pottigari Shashank Reddy <shashankreddypottigari137@gmail.com>
2026-06-29 16:48:10 +0530

    Added HTML and CSS frontend

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
+// const express = require("express");
+
+// const app = express();
+
+// app.get("/", (req,res)=>{
+//     res.send("Web Application Testing Pipeline");
+// });
+
+// app.listen(3000,()=>{
+//     console.log("Server Running");
+// });
+
+
+
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.get("/", (req,res)=>{
-    res.send("Web Application Testing Pipeline");
-});
+app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3000,()=>{
-    console.log("Server Running");
+app.listen(3000, () => {
+    console.log("Server Running on Port 3000");
 });
