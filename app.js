0b60418c109685a03e3e00fec78f7e614f5db9e4
Pottigari Shashank Reddy <shashankreddypottigari137@gmail.com>
2026-06-29 17:04:25 +0530

    Removed frontend files

--- app.js
+++ app.js
@@ -1,23 +1,10 @@
-// const express = require("express");
-
-// const app = express();
-
-// app.get("/", (req,res)=>{
-//     res.send("Web Application Testing Pipeline");
-// });
-
-// app.listen(3000,()=>{
-//     console.log("Server Running");
-// });
-
-
-
 const express = require("express");
-const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+    res.send("Web Application Testing Pipeline");
+});
 
 app.listen(3000, () => {
     console.log("Server Running on Port 3000");
